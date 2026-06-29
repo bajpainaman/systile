25cc7367b06e989505bcf32a0b96ffe5e00135d4
Naman Bajpai <95096343+bajpainaman@users.noreply.github.com>
2026-06-29 12:09:16 -0700

    test(geometry): tpu_v_geometry_dims

--- tests/geometry_tests.rs
+++ tests/geometry_tests.rs
@@ -3,3 +3,11 @@
 use systile::error::LatticeError;
 use systile::{Geometry, Shape};
 
+#[test]
+fn tpu_v_geometry_dims() {
+    let g = Geometry::TPU_V;
+    assert_eq!(g.sublanes, 8);
+    assert_eq!(g.lanes, 128);
+    assert_eq!(g.mxu, 128);
+}
+
