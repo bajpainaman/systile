0b5c5918c7f79f5d828f89d96b708c5e5e2a0801
Naman Bajpai <95096343+bajpainaman@users.noreply.github.com>
2026-06-29 12:38:54 -0700

    test(layout): offset_of_origin_is_zero

--- tests/layout_tests.rs
+++ tests/layout_tests.rs
@@ -4,3 +4,10 @@ use systile::layout::Layout;
 use systile::mask::Mask;
 use systile::{Geometry, PaddedTileLattice, Shape};
 
+#[test]
+fn offset_of_origin_is_zero() {
+    let shape = Shape::new(8, 128, &Geometry::TPU_V);
+    let layout = Layout::new(&shape, &Geometry::TPU_V);
+    assert_eq!(layout.offset(0, 0), 0);
+}
+
