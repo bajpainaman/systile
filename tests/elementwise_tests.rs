57e51f1e09d183ac55284a1b1a2f45b8210cd4dc
Naman Bajpai <95096343+bajpainaman@users.noreply.github.com>
2026-06-29 13:21:51 -0700

    test(elementwise): map_applies_to_logical_only

--- tests/elementwise_tests.rs
+++ tests/elementwise_tests.rs
@@ -3,3 +3,11 @@
 use systile::error::LatticeError;
 use systile::{Geometry, PaddedTileLattice};
 
+#[test]
+fn map_applies_to_logical_only() {
+    let l = PaddedTileLattice::from_dense(2, 3, &[1.0, 2.0, 3.0, 4.0, 5.0, 6.0], Geometry::TPU_V)
+        .unwrap();
+    let doubled = l.map(|x| x * 2.0);
+    assert_eq!(doubled.to_dense(), vec![2.0, 4.0, 6.0, 8.0, 10.0, 12.0]);
+}
+
