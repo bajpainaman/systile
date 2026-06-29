9abbe70a270026d28f00fbb6cbee1734eee9aa32
Naman Bajpai <95096343+bajpainaman@users.noreply.github.com>
2026-06-29 13:07:47 -0700

    test(sparse): all_zero_tile_is_detected

--- tests/sparse_tests.rs
+++ tests/sparse_tests.rs
@@ -2,3 +2,9 @@
 
 use systile::{Geometry, PaddedTileLattice};
 
+#[test]
+fn all_zero_tile_is_detected() {
+    let l = PaddedTileLattice::<f32>::zeroed(2, 2, Geometry::TINY).unwrap();
+    assert!(l.is_tile_zero(0, 0));
+}
+
