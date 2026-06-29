0600feda011e569ef6f976876d669346cdab8c31
Naman Bajpai <95096343+bajpainaman@users.noreply.github.com>
2026-06-29 13:15:11 -0700

    test(transpose): transpose_swaps_dims

--- tests/transpose_tests.rs
+++ tests/transpose_tests.rs
@@ -2,3 +2,11 @@
 
 use systile::{Geometry, PaddedTileLattice};
 
+#[test]
+fn transpose_swaps_dims() {
+    let l = PaddedTileLattice::from_dense(2, 3, &[1.0; 6], Geometry::TPU_V).unwrap();
+    let t = l.transpose();
+    assert_eq!(t.rows(), 3);
+    assert_eq!(t.cols(), 2);
+}
+
