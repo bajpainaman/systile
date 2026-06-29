3e6059103c3316a6326a11a4eec019475717acba
Naman Bajpai <95096343+bajpainaman@users.noreply.github.com>
2026-06-29 13:27:47 -0700

    feat(reduce): add sample()

--- tests/reduce_tests.rs
+++ tests/reduce_tests.rs
@@ -2,3 +2,7 @@
 
 use systile::{Geometry, PaddedTileLattice};
 
+fn sample() -> PaddedTileLattice<f32> {
+    PaddedTileLattice::from_dense(2, 3, &[1.0, 2.0, 3.0, 4.0, 5.0, 6.0], Geometry::TPU_V).unwrap()
+}
+
