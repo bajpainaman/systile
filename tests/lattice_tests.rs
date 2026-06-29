f4c731ae69b1e1fe5e95253f057af3f358f7f54c
Naman Bajpai <95096343+bajpainaman@users.noreply.github.com>
2026-06-29 12:23:20 -0700

    feat(lattice): add sample()

--- tests/lattice_tests.rs
+++ tests/lattice_tests.rs
@@ -3,3 +3,7 @@
 use systile::error::LatticeError;
 use systile::{Geometry, PaddedTileLattice};
 
+fn sample() -> PaddedTileLattice<f32> {
+    PaddedTileLattice::from_dense(2, 3, &[1.0, 2.0, 3.0, 4.0, 5.0, 6.0], Geometry::TPU_V).unwrap()
+}
+
