01bed9cdc6acecfe892895e0a618944962fcef8a
Naman Bajpai <95096343+bajpainaman@users.noreply.github.com>
2026-06-29 13:24:05 -0700

    test(elementwise): zip_with_adds

--- tests/elementwise_tests.rs
+++ tests/elementwise_tests.rs
@@ -25,3 +25,11 @@ fn map_in_place_mutates() {
     assert_eq!(l.to_dense(), vec![2.0, 3.0, 4.0]);
 }
 
+#[test]
+fn zip_with_adds() {
+    let a = PaddedTileLattice::from_dense(1, 3, &[1.0, 2.0, 3.0], Geometry::TPU_V).unwrap();
+    let b = PaddedTileLattice::from_dense(1, 3, &[10.0, 20.0, 30.0], Geometry::TPU_V).unwrap();
+    let c = a.zip_with(&b, |x, y| x + y).unwrap();
+    assert_eq!(c.to_dense(), vec![11.0, 22.0, 33.0]);
+}
+
